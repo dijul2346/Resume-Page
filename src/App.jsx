d26f86f2f8beb7ec70dc2a01422034d3ed480e18
dijul2346 <146112003+dijul2346@users.noreply.github.com>
2025-07-07 23:03:40 +0530

    Created a resume using tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,145 @@
+function App(){
+  return(
+    <div class="bg-white  justify-self-center w-5/8 p-10 pl-20 mt-20 mb-20 rounded shadow-2xl mx-auto border ">
+      <h1 class="text-4xl font-bold text-gray-800 mb-2">Dijul M</h1>
+      <h2 class="text-xl text-gray-800 mb-2">Student | Developer</h2>
+      <h3 class="text-l text-gray-800 mb-2">+91 6282117500 | dijul897@gmail.com </h3>
+      <div class="flex gap-10">
+      
+      <span class="[&>svg]:h-5 [&>svg]:w-5 flex gap-1">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"viewBox="0 0 448 512">
+      <path d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z" />
+      </svg>
+      <a src="https://in.linkedin.com/in/dijul-m-519a07254">  Dijul M</a>
+      </span>
+
+      <span class="[&>svg]:h-5 [&>svg]:w-5 flex gap-1">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 496 512">
+      <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+      </svg>
+      <a href="https://github.com/dijul2346"> dijul2346</a>
+      </span>
+      </div>
+
+      
+      
+      <h2 class="text-xl text-gray-800 mb-2 pt-5">Summary</h2>
+
+      <div class="w-4/5"><p class="text-xs">Motivated and solution-oriented B.Tech Computer Science student at MITS with a strong foundation in full-stack development, machine learning, and cloud technologies. Experienced in building socially relevant tech solutions, including real-time animal detection systems and fall detection for elderly care. Skilled in React, Flutter, Firebase, Python, and SQL. Certified in Google Cloud and Data Science, with hands-on project experience in both academic and real-world scenarios.</p></div>
+      
+      <div className="max-w-3xl  bg-white rounded-2xl">
+      <h2 class="text-xl text-gray-800 mb-2 pt-5">Skills</h2>
+
+  <div className="grid grid-cols-2 md:grid-cols-3 gap-4 text-sm text-gray-700">
+    <div>
+      <h3 className="font-semibold text-gray-900">Languages</h3>
+      <ul className="list-disc list-inside">
+        <li>Python</li>
+        <li>JavaScript</li>
+        <li>C</li>
+        <li>SQL</li>
+      </ul>
+    </div>
+
+    <div>
+      <h3 className="font-semibold text-gray-900">Web & App</h3>
+      <ul className="list-disc list-inside">
+        <li>React.js</li>
+        <li>Node.js</li>
+        <li>Flutter</li>
+        <li>HTML/CSS</li>
+      </ul>
+    </div>
+
+    <div>
+      <h3 className="font-semibold text-gray-900">Backend & DB</h3>
+      <ul className="list-disc list-inside">
+        <li>MongoDB</li>
+        <li>Firebase</li>
+        <li>MySQL</li>
+        <li>Prisma ORM</li>
+      </ul>
+    </div>
+
+    <div>
+      <h3 className="font-semibold text-gray-900">Tools & Platforms</h3>
+      <ul className="list-disc list-inside">
+        <li>Git & GitHub</li>
+        <li>Postman</li>
+        <li>GCP</li>
+        <li>VS Code</li>
+      </ul>
+    </div>
+
+    <div>
+      <h3 className="font-semibold text-gray-900">ML & AI</h3>
+      <ul className="list-disc list-inside">
+        <li>YOLOv8</li>
+        <li>OpenCV</li>
+        <li>Power BI</li>
+      </ul>
+    </div>
+
+    <div>
+      <h3 className="font-semibold text-gray-900">Soft Skills</h3>
+        <ul className="list-disc list-inside">
+          <li>Teamwork</li>
+          <li>Problem-solving</li>
+          <li>Communication</li>
+          <li>Leadership</li>
+        </ul>
+      </div>
+      </div>
+    </div>
+    <div className="max-w-3xl  bg-white  ">
+      <h2 className="text-xl text-gray-800 mb-2 pt-5">Education</h2>
+      <div className="text-sm text-gray-700">
+        <h3 className="font-semibold text-gray-900">B.Tech in Computer Science and Engineering</h3>
+        <ul className="list-disc list-inside">
+          <li> Muthoot Institute of Technology and Science 2022-26</li>
+          <li>CGPA 7.97</li>
+          <li>Positions : Class representative, AV Team member at Media Club,</li>
+          <li> Membership Team Member at ACM</li>
+        </ul>
+        <h3 className="font-semibold text-gray-900 pt-5">Higher Secondary</h3>
+        <ul className="list-disc list-inside">
+          <li>Kendriya Vidyalaya No-1 Kozhikode 2020-22</li>
+          <li>91%</li>
+        </ul>
+      </div>
+    </div>
+    <div className="max-w-3xl  bg-white  ">
+      <h2 className="text-xl text-gray-800 mb-2 pt-5">Projects</h2>
+      <div className="text-sm text-gray-700">
+        <h3 className="font-semibold text-gray-900">Wild-Eye</h3>
+        <ul className="list-disc list-inside">
+          <li>YOLOv8 (object detection) | Python (model implementation and video processing) <br /> Flutter
+ (frontend development) | Firebase (backend for data storage)</li>
+          <li> Developing a mobile application that
+ enhances safety by providing real-time alerts about nearby wild animals. Powered by the YOLO v11
+ model for efficient and accurate animal detection, this app aims to bridge the gap between technology
+ and wildlife conservation. By delivering instant notifications, our solution ensures user safety while
+ promoting peaceful coexistence with nature</li>
+          
+        </ul>
+        <h3 className="font-semibold text-gray-900 pt-5"> Disaster Rescue System App</h3>
+        <ul className="list-disc list-inside">
+          <li>Designed and developed a comprehensive disaster rescue system application leveraging SQL for robust data management. The app streamlines communication and coordination among rescue teams, victims, and authorities during emergencies. Key features include real-time location tracking, automated SOS alerts, resource allocation, and a centralized dashboard for monitoring ongoing rescue operations. The system ensures efficient information flow, quick response times, and improved safety for affected individuals by integrating user-friendly interfaces and secure data handling.</li>
+        </ul>
+        <h3 className="font-semibold text-gray-900 pt-5">Blog-Space</h3>
+        <ul className="list-disc list-inside">
+          <li>Developed "Blog Space," a full-stack blogging platform using Node.js, Express, MongoDB, and React. Implemented secure user authentication with JWT, RESTful APIs, and input validation with Zod. Designed for scalable, modern web application development with environment-based configuration and robust backend architecture.</li>
+        </ul>
+      </div>
+    </div>
+    
+
+
+
+      
+
+    </div>
+  )
+}
+
+export default App;
